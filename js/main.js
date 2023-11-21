14d35208893f5e6dec1ea99f343638d1c44af881
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-21 15:57:02 +0100

    Milestone 0 and 1

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// MILESTONE 0
+
+let array = [
+    {
+        fullName: "Tizio Caio 1",
+        role: "Developer",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        fullName: "Tizio Caio 2",
+        role: "Developer",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        fullName: "Tizio Caio 3",
+        role: "Developer",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        fullName: "Tizio Caio 4",
+        role: "Developer",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        fullName: "Tizio Caio 5",
+        role: "Developer",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        fullName: "Tizio Caio 6",
+        role: "Developer",
+        image: "angela-caroll-chief-editor.jpg"
+    }
+];
+
+
+// MILESTONE 1
+
+// Dato un array di oggetti, stampiamo i campi di ogni oggetto.
+function printArrayObjects(arrayOfObject) {
+    for (let key in arrayOfObject) {
+        console.log(key)
+    }
+}
+
+
+
